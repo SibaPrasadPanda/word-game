47b29f1a27371963548235eacaddaa10b3da46a4
Siba Prasad Panda <sibaprasadpanda100@gmail.com>
2025-05-04 14:21:23 +0530

    updated

--- frontend/src/environments/environment.ts
+++ frontend/src/environments/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://word-game-rt8y.onrender.com/',
+  apiUrl: 'https://word-game-rt8y.onrender.com',
   supabaseUrl: 'https://kzdvsokfiaqoyrchwowk.supabase.co',
   supabaseKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imt6ZHZzb2tmaWFxb3lyY2h3b3drIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDYyODYyNTgsImV4cCI6MjA2MTg2MjI1OH0.kVvwAhTHCHl186t7CVGxQt3SNPAPUBfKp_53trVUOWI'
 };
